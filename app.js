332f6cb612b1d77db019ddb1481fb812bd5d98ad
Jonathan Hudak <hudak.jonathan@gmail.com>
2019-02-17 12:34:03 -0800

    learnin gkoa and up

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
-const http = require('http')
-const { PORT = 3000 } = process.env
+const Koa = require("koa");
+const koaBody = require("koa-body");
+const AWS = require("aws-sdk");
+const bucketName = "anni-jmh";
+const Router = require("koa-router");
 
-http.createServer((req, res) => {
-  res.end('Hello jhudak\n')
-}).listen(PORT)
+const { PORT = 3000 } = process.env;
+const app = new Koa();
+const s3 = new AWS.S3();
+const router = new Router();
+
+async function saveJSONFile(
+  fileName = "database.json",
+  Body = '{ "foo": "bar" }'
+) {
+  var params = {
+    Bucket: bucketName,
+    Key: fileName,
+    Body,
+    ACL: "public-read",
+    region: "us-west-2"
+  };
+  await s3.putObject(params, function(err, data) {
+    if (err) console.log(err);
+    else
+      console.log(
+        "Successfully uploaded data to " + bucketName + "/" + fileName
+      );
+  });
+}
+
+app
+  .use(
+    koaBody({
+      jsonLimit: "1kb"
+    })
+  )
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+// app.use(async ctx => {
+//   const body = ctx.request.body;
+//   console.log(body.name);
+//   if (!body.name) ctx.throw(400, ".name required");
+//   ctx.body = { name: body.name.toUpperCase() };
+// });
+
+router.post("/projects", async (ctx, next) => {
+  const body = ctx.request.body;
+  await saveJSONFile(body.name, JSON.stringify(body.data));
+  ctx.body = "done?";
+});
+
+app.listen(PORT);
